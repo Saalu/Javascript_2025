33bd7bf04b752aaa664f799f33c4281279a7037b
saalu issaka <saaluissaka@gmail.com>
2025-07-30 20:23:44 +0000

    data control

---
+++ lesson1/app.js
@@ -0,0 +1,6 @@
+const fruits = ['mango', 'orange', 'pineapple', 'apple']
+
+fruits.forEach(function(juice, index){
+    console.log(`${[index]} : ${juice}`);
+    
+})
---
+++ lesson1/prev.js
@@ -0,0 +1,26 @@
+const today = new Date()
+
+// console.log(today)
+
+// const birthday = new Date('April 11, 1988')
+// console.log(birthday)
+
+const score = 1000
+if(score == '100'){
+    console.log('Yes, they\'re the same'); 
+}else{
+    console.log('No, they\'re different');
+}
+
+// check odd or even
+
+for (let i = 0; i < 11; i++){
+    if(i % 2){
+        console.log(`Odd Number: ${i}`);
+
+    }else{
+        console.log(`Even Number: ${i}`);
+
+    }
+    
+}
