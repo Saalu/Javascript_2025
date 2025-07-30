7b57997f7920cf4012e02e8390a701d9cdb8393d
saalu issaka <saaluissaka@gmail.com>
2025-07-30 20:35:01 +0000

    lesson-objects

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,6 +1,19 @@
 const fruits = ['mango', 'orange', 'pineapple', 'apple']
 
 fruits.forEach(function(juice, index){
-    console.log(`${[index]} : ${juice}`);
-    
+    // console.log(`${[index]} : ${juice}`);  
 })
+
+const person = {
+  name: "Kwame",
+  age: 30,
+  location: "Accra",
+  occupation: "Developer",
+  hobbies: ["coding", "cycling", "chess"]
+};
+
+// console.log(person.hobbies);
+for (let key in person){
+    console.log(`${person[key]}`);
+    
+}
