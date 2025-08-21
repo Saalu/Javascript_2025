949bd0c7fe776173723783beb5cd59fccfea28d5
saalu issaka <saaluissaka@gmail.com>
2025-08-21 10:31:34 +0000

    Fetch and arrow functions

--- lesson8/app.js
+++ lesson8/app.js
@@ -1,7 +1,7 @@
 
 document.getElementById('btn1').addEventListener('click', loadText)
 document.getElementById('btn2').addEventListener('click', loadJSON)
-document.getElementById('btn3').addEventListener('click', loadText)
+document.getElementById('btn3').addEventListener('click', loadRest)
 
 function loadText(){
     fetch('data.txt')
@@ -26,6 +26,30 @@ function loadJSON(){
         let html = '';
         data.forEach(item => {
             html += `<li>${item.name} -- ${item.position} </li>`
+        });
+        document.querySelector('#result').innerHTML = html
+    })
+    .catch((err)=>{
+        console.log(err);     
+    })
+}
+
+// https://picsum.photos/list
+function loadRest(){
+    fetch('https://picsum.photos/list')
+    .then((res) => res.json() )
+    .then((data)=>{
+        let html = '';
+        const limitedData = data.slice(0, 10);
+        console.log(limitedData);
+        
+        limitedData.forEach(image => {
+            html += `
+            <div>
+            <li><a href =${image.author_url}>View Image</a> ${image.author}</li>
+            <img src=${image.filename} alt="">
+            </div>
+            `
         });
         document.querySelector('#result').innerHTML = html
     })
