d2af7627452980505e327c4db05491c363b8e129
saalu issaka <saaluissaka@gmail.com>
2025-09-10 11:07:43 +0000

    part-1 callbacks

---
+++ lesson11/app.js
@@ -0,0 +1,35 @@
+// document.getElementById('btn1').addEventListener('click', loadText)
+// document.getElementById('btn2').addEventListener('click', loadJSON)
+// document.getElementById('btn3').addEventListener('click', loadRest)
+
+// callbacks = {
+//     they function within another fucntion
+// }
+
+const countries = ['Ghana', 'Chad', 'Nigeria', 'Somalia', 'Liberia']
+const result = document.getElementById('result')
+
+function newCountry(country, callback){
+    setTimeout(() => {
+        countries.push(country)
+        callback()
+    }, 2000);
+}
+
+function displayCountry(){
+
+    setTimeout(function(){
+        let html = '';
+
+        countries.forEach(country => {
+            html += `<li>${country}</li>`
+        });
+        document.getElementById('result').innerHTML = html
+    },1000)
+    // result.appendChild = html
+}
+
+newCountry('Togo', displayCountry)
+
+displayCountry()
+
