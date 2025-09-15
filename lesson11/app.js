0f0cdd325afc21904ea190284aa2d85cbb7b4e17
saalu issaka <saaluissaka@gmail.com>
2025-09-15 17:17:17 +0000

    Job card design and data fetching

--- lesson11/app.js
+++ lesson11/app.js
@@ -1,54 +1,60 @@
-document.getElementById('btn1').addEventListener('click', loadText)
-document.getElementById('btn2').addEventListener('click', loadJSON)
-document.getElementById('btn3').addEventListener('click', loadAPI)
+// document.getElementById('btn1').addEventListener('click', loadText)
+// document.getElementById('btn2').addEventListener('click', loadJSON)
+// document.getElementById('btn3').addEventListener('click', loadAPI)
 
-const result = document.getElementById('result')
+const output = document.getElementById('result')
 
-function loadText(){
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btn3 = document.getElementById('btn3')
+
+btn1.addEventListener('click', function(){
+    // console.log('Hello world');
     fetch('data.txt')
-    .then(res =>  {
-        return res.text()
-        console.log(res.text());
-        
-    }).then(data =>{
-        console.log(data);
-        result.innerHTML = data
-    })
-}
+    .then((res) => res.text())
+    .then(data => console.log(data))
+    .catch(err => console.log(err.message))  
+})
 
-function loadJSON ()  {
-    fetch('data.json')
-    .then(res => res.json())
-    .then(data => {   
+// btn2.addEventListener('click', function(){
+//     console.log('Fetching JSON data...');
+    
+//     fetch('data.json')
+//     .then(res => res.json() )
+//     .then(data =>{ 
+//         let html = '';
+//         data.forEach(element => {
+//             // console.log(element.position)
+//             html += `<li class='output'>
+//                     Name: ${element.name} - <a href='#'>${element.position}</a>  
+//             </li>`
+//         })
 
-        let html = ''
+//         output.innerHTML = html
+//     })
+//     .catch(err => console.log(err.message))
+// })
 
-        data.forEach(element => {
-            html += `<li> ${element.name}  - ${element.position}</li>`
-        });
-        result.innerHTML = html
-    }).catch(err => console.log(err.message))
-
-}
-
-async function loadAPI () {
-   const res = await fetch('https://picsum.photos/list')
-
+ btn2.addEventListener('click', async function(){
+    console.log(' Refactoring the Fetching JSON data...');
+    
+    const res = await fetch('data.json')
     try {
-      const data = res.json()
-      .then(data => {
-        const limitData = data.slice( 173, 178); 
-        console.log(limitData)
-
+       const data = res.json()
+       .then( data => data)
+    .then(data =>{ 
         let html = '';
-        limitData.forEach(element =>{
-
-            html += `<li>Author: ${element.author}  <a href=${element.post_url}>View </a> </li>`
+        data.forEach(element => {
+            // console.log(element.position)
+            html += `<li class='output'>
+                    Agent: ${element.name} - <a href='#'>${element.position}</a>  
+            </li>`
         })
-        result.innerHTML = html
-      })
-      
+
+        output.innerHTML = html
+    })
     } catch (err) {
-        console.log(err.message)    
-    }
-}
+        console.log(err.message)
+    } 
+   
+})
--- lesson11/index.js
+++ lesson11/index.js
@@ -27,3 +27,62 @@ newCountry('Togo', displayCountry)
 
 displayCountry()
 
+
+
+
+// PART 2
+
+document.getElementById('btn1').addEventListener('click', loadText)
+document.getElementById('btn2').addEventListener('click', loadJSON)
+document.getElementById('btn3').addEventListener('click', loadAPI)
+
+const output = document.getElementById('result')
+
+function loadText(){
+    fetch('data.txt')
+    .then(res =>  {
+        return res.text()
+        console.log(res.text());
+        
+    }).then(data =>{
+        console.log(data);
+        result.innerHTML = data
+    })
+}
+
+function loadJSON ()  {
+    fetch('data.json')
+    .then(res => res.json())
+    .then(data => {   
+
+        let html = ''
+
+        data.forEach(element => {
+            html += `<li> ${element.name}  - ${element.position}</li>`
+        });
+        result.innerHTML = html
+    }).catch(err => console.log(err.message))
+
+}
+
+async function loadAPI () {
+   const res = await fetch('https://picsum.photos/list')
+
+    try {
+      const data = res.json()
+      .then(data => {
+        const limitData = data.slice( 173, 178); 
+        console.log(limitData)
+
+        let html = '';
+        limitData.forEach(element =>{
+
+            html += `<li>Author: ${element.author}  <a href=${element.post_url}>View </a> </li>`
+        })
+        result.innerHTML = html
+      })
+      
+    } catch (err) {
+        console.log(err.message)    
+    }
+}
