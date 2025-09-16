52df4fecdd21850d2d2305336841a1553ede01ce
saalu issaka <saaluissaka@gmail.com>
2025-09-16 10:02:09 +0000

    Improved job card

--- lesson11/app.js
+++ lesson11/app.js
@@ -2,7 +2,7 @@
 // document.getElementById('btn2').addEventListener('click', loadJSON)
 // document.getElementById('btn3').addEventListener('click', loadAPI)
 
-const output = document.getElementById('result')
+const output = document.getElementById('container')
 
 const btn1 = document.getElementById('btn1')
 const btn2 = document.getElementById('btn2')
@@ -46,9 +46,26 @@ btn1.addEventListener('click', function(){
         let html = '';
         data.forEach(element => {
             // console.log(element.position)
-            html += `<li class='output'>
-                    Agent: ${element.name} - <a href='#'>${element.position}</a>  
-            </li>`
+            html += `
+                <div class="wrapper">
+                    <div id="result">
+                        <div class="img-container">
+                        <img src="sample.png" alt="img  here">
+                        </div>
+                    <div class="details">
+                        <div class="vital">
+                        <h3>${element.name}</h3>
+                        <p>${element.title}</p>
+                    </div>
+                    <div class="add-on">
+                        </div>
+                        <li>Employee ID  : 12345678</li>
+                        <li>Issue Date   : 01/01/2050</li>
+                        <li>Valid until  : 01/01/2051</li>
+                    </div>
+                    </div>
+                </div>
+            `
         })
 
         output.innerHTML = html
