d3ebb59cbb8c6c68dd7992f9aef59c3d1567b429
saalu issaka <saaluissaka@gmail.com>
2025-09-22 14:29:36 +0000

    Portfolio: Navbar and Home section

--- project1/app.js
+++ project1/app.js
@@ -1,14 +1,19 @@
 let lastScrollTop = 0;
-const navbar = document.getElementById("navbar");
+const navbar = document.querySelector(".header .navigation");
 
 window.addEventListener("scroll", function() {
     let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
     if (currentScroll > lastScrollTop) {
+        console.log('Go Down');
         // Scroll down, hide the navbar
-        navbar.style.top = "-50px"; // Height of the navbar
+        navbar.classList.remove('active')
+        navbar.style.top = "-8.5rem"; // Height of the navbar
+    
     } else {
+        console.log('Go Up');
         // Scroll up, show the navbar
+        navbar.classList.add('active')
         navbar.style.top = "0";
     }
 
